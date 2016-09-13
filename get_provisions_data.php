027ea56f8b456c5c1aad17a455a9d51f62fdea3b
Pradeep Kumar <prakumk@gmail.com>
2016-09-13 11:54:30 +0530

    Create get_provisions_data.php

---
+++ get_provisions_data.php
@@ -0,0 +1,37 @@
+[
+{
+	"type":0,
+	"name":"Desktop",
+	"code":"A321YR",
+	"alloc_on":"02/03/2016",
+	"prov_manager":"Vinayak Gupta"
+},
+{
+	"type":1,
+	"name":"Laptop",
+	"code":"B674SJ",
+	"alloc_on":"08/06/2016",
+	"prov_manager":"Chayan Mukhopadhyay"
+},
+{
+	"type":2,
+	"name":"Mobile",
+	"code":"C435LK",
+	"alloc_on":"25/08/2016",
+	"prov_manager":"Himanshu Aggarwal"
+},
+{
+	"type":3,
+	"name":"Cabin",
+	"code":"D875SK",
+	"alloc_on":"13/07/2016",
+	"prov_manager":"Rakesh Kumar"
+},
+{
+	"type":4,
+	"name":"Pen Drive",
+	"code":"E234HJ",
+	"alloc_on":"26/02/2016",
+	"prov_manager":"Rajendar Sir"
+}
+]
