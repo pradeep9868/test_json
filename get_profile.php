1cc348c54d705254609f65b8460cb002de4511f4
Pradeep Kumar <prakumk@gmail.com>
2016-09-13 11:55:36 +0530

    Create get_profile.php

---
+++ get_profile.php
@@ -0,0 +1,13 @@
+{
+	
+	"name":"Pradeep Kumar",
+	"position":"Android Developer",
+	"location":"Gurgaon,India",
+	"image":"http://xyz.com",
+	"contact_details":[
+	{"title":"Personal Email","value":"pradeep.kushwaha@qandle.com"},
+	{"title":"Work Phone no","value":"120 - 1245632"},
+	{"title":"Mobile no","value":"+91 7838310825"},
+	{"title":"Home Phone no","value":"+91 9868458422"}
+	]
+}
