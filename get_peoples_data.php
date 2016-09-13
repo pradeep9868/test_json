0b97273464893b97049f35c554d1be2ab0d4ea87
Pradeep Kumar <prakumk@gmail.com>
2016-09-13 11:55:58 +0530

    Create get_peoples_data.php

---
+++ get_peoples_data.php
@@ -0,0 +1,48 @@
+{
+"organisation_strength":952,
+"team_strength":34,
+ "peer_images":[
+ {"url":"https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAS0AAAAJGZlNzUzZWI4LTliOTQtNDAwNC04ZmFkLTIzOTQ2MDZjNWFhMA.jpg","userid":"3746823DGD2324"},
+ {"url":"https://media0.giphy.com/media/TlK63EuTL8tjEdonXig/200_s.gif","userid":"SDGFEWTUEW437Y3HFU"},
+ {"url":"http://m5.paperblog.com/i/140/1408987/the-face-of-the-deep-exploring-the-mysterious-L-kHHQwa.jpeg","userid":"UHDIUE3Y7834Y348E"},
+ {"url":"http://ranbloo.com/wp-content/uploads/2011/06/Brad-Pitt.png","userid":"743HRR34876437HDUE"},
+ {"url":"https://media0.giphy.com/media/KBfIW3tWKios8/200_s.gif","userid":"UEU3YR87433487Y439438"},
+ {"url":"https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/8/005/0a8/06f/12e05d1.jpg","userid":"EY89EFER988ER"},
+ {"url":"https://lh3.googleusercontent.com/-1s7YTFho3EI/UmVdSf0UVcI/AAAAAAAABSI/fTGhb_vGQS8/w1454-h1449/Anil.jpg","userid":"ERUY8ER8934"},
+ {"url":"https://pbs.twimg.com/profile_images/537336622535016448/WLbJNqKM.jpeg","userid":"WDERYUE378434"},
+ {"url":"http://bsmedia.business-standard.com/_media/bs/img/article/2015-08/04/full/1438684070-8769.jpg","userid":"DYFUE6R87RY3489"},
+ {"url":"https://i1.rgstatic.net/ii/profile.image/AS%3A272461967130636@1441971403567_m/Anil_Kumar_K_M.png","userid":"EUIRYR8HRE98RE"}
+ ],
+ 
+ "maps_data":[{
+	 "x":46,
+	 "y":15,
+	 "data":["New Delhi    302","Gurgaon    132"]
+ },
+ {
+	 "x":33,
+	 "y":9,
+	 "data":["London    112"]
+ },
+ {
+	 "x":13,
+	 "y":15,
+	 "data":["U.S.A    67"]
+ },
+ {
+	 "x":47,
+	 "y":23,
+	 "data":["Sri Lanka    236"]
+ },
+ {
+	 "x":28,
+	 "y":8,
+	 "data":["IceLand    27"]
+ },
+ {
+	 "x":60,
+	 "y":31,
+	 "data":["Sydney    13"]
+ }
+ ]
+}
