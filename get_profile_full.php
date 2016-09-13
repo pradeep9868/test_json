2f69187a89b2cb9d21e663a598473e4db131a579
Pradeep Kumar <prakumk@gmail.com>
2016-09-13 11:55:11 +0530

    Create get_profile_full.php

---
+++ get_profile_full.php
@@ -0,0 +1,11 @@
+{
+	"contact_details":[
+	{"title":"Personal Email","value":"pradeep.kushwaha@qandle.com"},
+	{"title":"Work Phone no","value":"120 - 1245632"},
+	{"title":"Mobile no","value":"+91 7838310825"},
+	{"title":"Home Phone no","value":"+91 9868458422"},
+	{"title":"Current Address","value":"D-82, Sangam Vihar,New Delhi"},
+	{"title":"Permanent Address","value":"KL House, Gurgaon,Haryana"},
+	{"title":"Highest Education","value":"B.Tech, GGSIPU"}
+	]
+}
