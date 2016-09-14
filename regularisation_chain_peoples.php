5894b37f0730ead3b750a94f94d6752b0ddd8557
Pradeep Kumar <prakumk@gmail.com>
2016-09-14 14:36:59 +0530

    Create regularisation_chain_peoples.php

---
+++ regularisation_chain_peoples.php
@@ -0,0 +1,17 @@
+[
+{
+"name":"Pradeep Kumar",
+"designation":"Android Developer",
+"Image":""
+},
+{
+"name":"Himanshu Aggarwal",
+"designation":"Tech Head",
+"Image":""
+},
+{
+"name":"Chayan",
+"designation":"Director",
+"Image":""
+}
+]
