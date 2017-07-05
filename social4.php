66959cee0e68a77cfce30ce70b70cbfaca04a7d0
Pradeep Kumar <prakumk@gmail.com>
2017-07-05 22:28:07 +0530

    Add files via upload

---
+++ social4.php
@@ -0,0 +1,218 @@
+{
+"status":"success",
+"data" : [
+{
+"emp_detail" : {
+"full_name": "Rahul Chhabra",
+"_id": "587f036439473eba7c6e5920",
+"profile_pic": "https://q.qandle.com/useraddition/image/587f89e039473ebb4d6e5920/22995.jpg",
+"crop_type": "1",
+"display_detail": [
+"Rahul Chhabra",
+"Product",
+"Manager Operations & Product"
+]
+},
+"post" : "#Entrepreneurs often underestimate the complexity of #HRFunctions and the field's requisite legal requirements - Investing in #HRMS could help. #SmartHR #DigitalTransformation #HRTech",
+"image" : "",
+"likes" : "20",
+"comment" : "3",
+"type" : "status",
+"created_time" : "2017-07-05T20:13:34+0000"
+},
+{
+"emp_detail" : {
+"full_name": "Arun Chandramohan",
+"_id": "587e4de639473e9602293027",
+"profile_pic": "https://q.qandle.com/useraddition/image/587f852d39473efe446e591f/60155.jpg",
+"crop_type": "1",
+"display_detail": [
+"Arun Chandramohan",
+"Managing Director",
+"Managing Director"
+]
+},
+"post" : "“You’ll attract the employees you need if you can explain why your mission is compelling: not why it’s important in general, but why you’re doing something important that no one else is going to get done.” – Peter Thiel #EmployeeEngagement #MissionVision",
+"image" : "http://image-store.slidesharecdn.com/9883d52e-1f23-41f2-9d74-ff79d7f5c6f2-large.jpeg",
+"likes" : "44",
+"comment" : "5",
+"type" : "photo",
+"created_time" : "2017-07-05T19:11:34+0000"
+},
+{
+"emp_detail" : {
+"full_name": "Chayan Mukhopadhyay",
+"_id": "587e49dc39473ee015293024",
+"profile_pic": "https://q.qandle.com/useraddition/image/587f867139473e2d3d6e5960/59242.jpg",
+"crop_type": "1",
+"display_detail": [
+"Chayan Mukhopadhyay",
+"Product, Business",
+"Director"
+]
+},
+"post" : "“Throw yourself into some work you believe in with all your heart, live for it, die for it, and you will find happiness that you had thought could never be yours.” – Dale Carnegie #EmployeeEngagement #OrgStrength",
+"image" : "http://image-store.slidesharecdn.com/e92d2a79-2b13-4a36-a101-42c7f8fb1d9f-large.jpeg",
+"likes" : "14",
+"comment" : "6",
+"type" : "photo",
+"created_time" : "2017-07-05T18:21:34+0000"
+},
+{
+"emp_detail" : {
+"full_name": "Pradeep Kumar Kushwaha",
+"_id": "587f3d0239473e4e1f6e5920",
+"profile_pic": "https://q.qandle.com/useraddition/image/587f913439473e73526e5923/95835.jpg",
+"crop_type": "1",
+"display_detail": [
+"Pradeep Kumar Kushwaha",
+"Technology",
+"Software Engineer"
+]
+},
+"post" : "I have always believed that the way you treat your employees is the way they will treat your customers, and that people flourish when they are praised - Richard Branson #Leadership #EmployeeEngagement",
+"image" : "http://image-store.slidesharecdn.com/eb7f7b04-4974-4a9c-9f8c-e0995276f34d-large.jpeg",
+"likes" : "32",
+"comment" : "13",
+"type" : "photo",
+"created_time" : "2017-07-05T16:17:34+0000"
+},
+{
+"emp_detail" : {
+"full_name": "Komal Choudhary",
+"_id": "587f048539473e247e6e591f",
+"profile_pic": "https://q.qandle.com/useraddition/image/587f8a7239473edc436e5922/41978.jpg",
+"crop_type": "1",
+"display_detail": [
+"Komal Choudhary",
+"Business",
+"Payroll & HR Specialist"
+]
+},
+"post" : "Increase #Transparency through #SelfService and providing pay slips, tax slips, PF/ESI statement, Form 16’s etc.",
+"image" : "",
+"likes" : "3",
+"comment" : "9",
+"type" : "status",
+"created_time" : "2017-07-05T16:01:34+0000"
+},
+{
+"emp_detail" : {
+"full_name": "Vipin Chandra Papnai",
+"_id": "587f02be39473e2d7d6e5920",
+"profile_pic": "https://q.qandle.com/useraddition/image/587f916c39473e57536e591f/46784.jpg",
+"crop_type": "1",
+"display_detail": [
+"Vipin Chandra Papnai",
+"Technology",
+"Software Engineer"
+]
+},
+"post" : "#HumanResources are like natural resources; they're often buried deep. You have to go looking for them, they're not just lying around on the surface. You have to create the circumstances where they show themselves.",
+"image" : "",
+"likes" : "4",
+"comment" : "3",
+"type" : "status",
+"created_time" : "2017-07-05T15:51:34+0000"
+},
+{
+"emp_detail" : {
+"full_name": "Gaurav Chaubey",
+"_id": "587f018039473eb77c6e5925",
+"profile_pic": "https://q.qandle.com/useraddition/image/587f934839473e334e6e5922/70872.",
+"crop_type": "1",
+"display_detail": [
+"Gaurav Chaubey",
+"Technology",
+"Software Engineer"
+]
+},
+"post" : "You can quibble with those numbers--and we will--because the human heart is one #Performance variable that's impossible to measure, despite the business world's #Obsession with metrics.",
+"image" : "https://www.incimages.com/uploaded_files/image/970x450/new-rules_44774.jpg",
+"likes" : "17",
+"link" : "https://www.inc.com/magazine/201412/paul-keegan/the-new-rules-of-engagement.html",
+"description" : "The 5 New Rules of Employee Engagement",
+"comment" : "13",
+"type" : "link",
+"created_time" : "2017-07-05T15:31:34+0000"
+},
+{
+"emp_detail" : {
+"full_name": "Rajendra Bhilwara",
+"_id": "587f012339473eb67c6e5920",
+"profile_pic": "https://q.qandle.com/useraddition/image/587f946439473e73526e5929/72380.png",
+"crop_type": "1",
+"display_detail": [
+"Rajendra Bhilwara",
+"Product",
+"UI/UX Designer"
+]
+},
+"post" : "As organizations become more #Digital, they face a growing imperative to redesign themselves to move faster, adapt more quickly and facilitate rapid #Learning. #SmartHR #DigitalTrends",
+"image" : "",
+"likes" : "26",
+"comment" : "2",
+"type" : "status",
+"created_time" : "2017-07-05T15:11:34+0000"
+},
+{
+"emp_detail" : {
+"full_name": "Jitender Singh",
+"_id": "587efff639473ea47b6e5924",
+"profile_pic": "https://q.qandle.com/useraddition/image/587f92ee39473e14546e591f/40654.jpg",
+"crop_type": "1",
+"display_detail": [
+"Jitender Singh",
+"Technology",
+"Software Engineer"
+]
+},
+"post" : "#HR is at a crossroads, as #Technology can now accomplish many of those traditional responsibilities faster, cheaper, and better than before. #SmartHR #DigitalTransformation",
+"image" : "",
+"likes" : "32",
+"comment" : "13",
+"source" : "https://www.youtube.com/watch?v=llTvxMRJsHQ",
+"type" : "video",
+"created_time" : "2017-07-05T14:41:34+0000"
+},
+{
+"emp_detail" : {
+"full_name": "Neetesh Kumar",
+"_id": "587eff7039473e7a7c6e591f",
+"profile_pic": "https://q.qandle.com/useraddition/image/587f92b439473e59536e5925/24927.jpg",
+"crop_type": "1",
+"display_detail": [
+"Neetesh Kumar",
+"Technology",
+"Software Engineer"
+]
+},
+"post" : "Digital and #ConsumerMarketing are permeating new ways of #Recruiting, working, learning, and #EngagingEmployees. #SmartHR #DigitalTransformation https://lnkd.in/fQAvctW",
+"image" : "",
+"likes" : "18",
+"comment" : "6",
+"type" : "status",
+"created_time" : "2017-07-05T13:51:34+0000"
+},
+{
+"emp_detail" : {
+"full_name": "Utkarsh Singh",
+"_id": "587efe6039473eaa7b6e591f",
+"profile_pic": "https://q.qandle.com/useraddition/image/587f926b39473e59536e5922/94186.jpg",
+"crop_type": "1",
+"display_detail": [
+"Utkarsh Singh",
+"Technology",
+"Software Engineer"
+]
+},
+"post" : "#HRMS also refers to the integration of #HumanResource management and #InformationTechnology to automate and facilitate human resource activities. https://lnkd.in/gGvBFjc",
+"image" : "",
+"likes" : "1",
+"comment" : "1",
+"type" : "status",
+"created_time" : "2017-07-05T13:11:34+0000"
+}
+
+]
+}
