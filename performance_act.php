f1b842b72deadfcec163e79e65f84b6e8fa08afa
Pradeep Kumar <prakumk@gmail.com>
2017-07-22 12:34:22 +0530

    Add files via upload

---
+++ performance_act.php
@@ -0,0 +1,61 @@
+{
+"status":"success",
+"data" : [
+{
+"owner" : {
+"full_name": "Rahul Chhabra",
+"_id": "587f036439473eba7c6e5920",
+"profile_pic": "https://q.qandle.com/useraddition/image/587f89e039473ebb4d6e5920/22995.jpg",
+"crop_type": "1",
+"display_detail": [
+"Rahul Chhabra",
+"Product",
+"Manager Operations & Product"
+]
+},
+"objective" : "Make a world class design Team",
+"key_result":3,
+"program_status":"3/4",
+"created" : "03/07/2017",
+"last_update" : "03/07/2017"
+},
+
+{
+"owner" : {
+"full_name": "Rahul Chhabra",
+"_id": "587f036439473eba7c6e5920",
+"profile_pic": "https://q.qandle.com/useraddition/image/587f89e039473ebb4d6e5920/22995.jpg",
+"crop_type": "1",
+"display_detail": [
+"Rahul Chhabra",
+"Product",
+"Manager Operations & Product"
+]
+},
+"objective" : "Make a world class development Team",
+"key_result":2,
+"program_status":"1/2",
+"created" : "03/06/2017",
+"last_update" : "03/06/2017"
+},
+
+{
+"owner" : {
+"full_name": "Rahul Chhabra",
+"_id": "587f036439473eba7c6e5920",
+"profile_pic": "https://q.qandle.com/useraddition/image/587f89e039473ebb4d6e5920/22995.jpg",
+"crop_type": "1",
+"display_detail": [
+"Rahul Chhabra",
+"Product",
+"Manager Operations & Product"
+]
+},
+"objective" : "Hire an Experienced HR",
+"key_result":2,
+"program_status":"1/2",
+"created" : "03/06/2017",
+"last_update" : "03/06/2017"
+}
+]
+}
