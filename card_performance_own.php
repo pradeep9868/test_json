abcff3314acb9b0762f15a08e699598208a571e2
Pradeep Kumar <prakumk@gmail.com>
2017-07-22 16:01:56 +0530

    Add files via upload

---
+++ card_performance_own.php
@@ -0,0 +1,33 @@
+{
+status: "success",
+message: "Employee Performance Own",
+data: {
+amount: 1572000,
+breakup: [
+{
+slug: "over_due",
+name: "Over Due",
+value: 18,
+per_payroll_cycle: ""
+},
+{
+slug: "in_progress",
+name: "In Progress",
+value: 16,
+per_payroll_cycle: ""
+},
+{
+slug: "on_time",
+name: "On Time",
+value: 17,
+per_payroll_cycle: ""
+},
+{
+slug: "completed",
+name: "Completed",
+value: 10,
+per_payroll_cycle: ""
+}
+]
+}
+}
---
+++ card_performance_team.php
@@ -0,0 +1,33 @@
+{
+status: "success",
+message: "Employee Performance Team",
+data: {
+amount: 1572000,
+breakup: [
+{
+slug: "over_due",
+name: "Over Due",
+value: 16,
+per_payroll_cycle: ""
+},
+{
+slug: "in_progress",
+name: "In Progress",
+value: 15,
+per_payroll_cycle: ""
+},
+{
+slug: "on_time",
+name: "On Time",
+value: 10,
+per_payroll_cycle: ""
+},
+{
+slug: "completed",
+name: "Completed",
+value: 13,
+per_payroll_cycle: ""
+}
+]
+}
+}
