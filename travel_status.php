e61a61511a9271e081ae13e6769c9e5e56aaf408
Pradeep Kumar <prakumk@gmail.com>
2017-07-23 18:57:27 +0530

    Add files via upload

---
+++ travel_status.php
@@ -0,0 +1,46 @@
+{
+"status": "success",
+"message": "Employee Travel Status",
+"data": [
+{
+"from" : "New Delhi",
+"to" : "Mumbai",
+"duration" : "28/06/2017 - 30/06/2017",
+"days" : 3,
+"nights" : 2,
+"req_date" : "26/06/2017",
+"reason" : "Business Trip",
+"status" : "Approved" 
+},
+{
+"from" : "Gurgaon",
+"to" : "Pune",
+"duration" : "21/05/2017 - 22/05/2017",
+"days" : 2,
+"nights" : 1,
+"req_date" : "13/05/2017",
+"reason" : "Conference",
+"status" : "Rejected" 
+},
+{
+"from" : "Mumbai",
+"to" : "New York",
+"duration" : "13/05/2017 - 16/05/2017",
+"days" : 4,
+"nights" : 3,
+"req_date" : "06/05/2017",
+"reason" : "Training",
+"status" : "Pending" 
+},
+{
+"from" : "Mumbai",
+"to" : "Mexico",
+"duration" : "03/03/2017 - 10/03/2017",
+"days" : 8,
+"nights" : 7,
+"req_date" : "22/02/2017",
+"reason" : "Expansion",
+"status" : "Closed" 
+}
+]
+}
