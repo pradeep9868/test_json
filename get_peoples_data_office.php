2f708182ded4af8aeec8d213cf37d43efab978bf
Pradeep Kumar <prakumk@gmail.com>
2017-07-28 11:39:03 +0530

    Add files via upload

---
+++ get_peoples_data_office.php
@@ -0,0 +1,33 @@
+{
+ "maps_data":[{
+	 "x":46,
+	 "y":15,
+	 "data":["New Delhi","Gurgaon"]
+ },
+ {
+	 "x":33,
+	 "y":9,
+	 "data":["London"]
+ },
+ {
+	 "x":13,
+	 "y":15,
+	 "data":["U.S.A"]
+ },
+ {
+	 "x":47,
+	 "y":23,
+	 "data":["Sri Lanka"]
+ },
+ {
+	 "x":28,
+	 "y":8,
+	 "data":["IceLand"]
+ },
+ {
+	 "x":60,
+	 "y":31,
+	 "data":["Sydney"]
+ }
+ ]
+}
