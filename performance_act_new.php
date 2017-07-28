08a0ea0f67d4068930a1676a2e7182c2f26747bb
Pradeep Kumar <prakumk@gmail.com>
2017-07-28 12:56:59 +0530

    Add files via upload

---
+++ performance_act_new.php
@@ -0,0 +1,23 @@
+{
+"status":"success",
+"data" : [
+{
+"owner" : {
+"full_name": "Rahul Chhabra",
+"_id": "587f036439473eba7c6e5920",
+"profile_pic": "https://q.qandle.com/useraddition/image/587f89e039473ebb4d6e5920/22995.jpg",
+"crop_type": "1",
+"display_detail": [
+"Rahul Chhabra",
+"Product",
+"Manager Operations & Product"
+]
+},
+"objective" : "Make a world class design Team",
+"key_result":4,
+"program_status":"3/4",
+"created" : "03/07/2017",
+"last_update" : "03/07/2017"
+}
+]
+}
